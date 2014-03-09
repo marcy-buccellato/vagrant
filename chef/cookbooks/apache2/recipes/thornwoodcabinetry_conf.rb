29bda5285ffd0b46174406546d6fe6a94e766d23
Marcy Petroccione <mpetroccione@noodle.org>
2014-03-09 13:35:12 -0400

    Add AllowOverride = All to recipe for thornwoodcabinetry apache conf to allow for symlinks

--- chef/cookbooks/apache2/recipes/thornwoodcabinetry_conf.rb
+++ chef/cookbooks/apache2/recipes/thornwoodcabinetry_conf.rb
@@ -2,4 +2,5 @@ web_app "thornwoodcabinetry.com" do
   server_name "www.thornwoodcabinetry.com"
   server_aliases [ "*.thornwoodcabinetry.com", "thornwoodcabinetry.com" ]
   docroot "/var/www/thornwood"
+  allow_override = 'All'
 end
